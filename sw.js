295623ef2430d8eb1a5e6a477f49f11b45404edf
Cisnemon13 <104937989+Cisnemon13@users.noreply.github.com>
2024-09-12 19:05:34 -0600

    Agregado mi nombre en el manifest

---
+++ sw.js
@@ -0,0 +1,27 @@
+console.log('SW Hola mundo !!');
+
+self.addEventListener('install',(event)=>{
+    console.log('SW: Instalado');
+});
+
+self.addEventListener('fetch', (event)=>{
+    console.log(event.request.url);
+    
+    
+
+    /*if(event.request.url.includes('style.css')){
+        const respuesta = new Response(
+            `body{
+                color: white;
+                background-color: #000;
+            }`,
+            {
+                headers:{
+                    'Content-Type':'text/css'
+                }
+            }
+        );
+        event.respondWith(respuesta);
+    }
+*/
+})
